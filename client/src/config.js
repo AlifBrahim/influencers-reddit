45bb947fefa0684d909940e8ca2cf2384c105af5
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2024-01-01 21:27:31 +0800

    change localhost to http://146.190.102.198/

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://post-it-heroku.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:3003/";
+  BASE_URL = "http://146.190.102.198:3003/";
 }
 
 export { BASE_URL };
