e0f34a3e4b153b3cab9b8a563e2433fa27d45962
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2024-01-04 19:43:15 +0800

    Change server.js to apply https route using SSL

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 const app = express();
 const { authSocket, socketServer } = require("./socketServer");
 const posts = require("./routes/posts");
@@ -13,11 +16,24 @@ const PostLike = require("./models/PostLike");
 const Post = require("./models/Post");
 
 dotenv.config();
+const PORT = process.env.NODE_ENV === 'production' ? 443 : 4000;
 
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer, {
+let server;
+
+if (process.env.NODE_ENV === 'production') {
+  const privateKey = fs.readFileSync('/home/alif/ssl/alifibrahim_me.key', 'utf8');
+  const certificate = fs.readFileSync('/home/alif/ssl/alifibrahim_me.crt', 'utf8');
+  const ca = fs.readFileSync('/home/alif/ssl/alifibrahim_me.ca-bundle', 'utf8');
+
+  const credentials = { key: privateKey, cert: certificate, ca: ca };
+  server = https.createServer(credentials, app);
+} else {
+  server = http.createServer(app);
+}
+
+const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://localhost:3003", "https://post-it-heroku.herokuapp.com"],
+    origin: ["http://localhost:3000", "https://146.190.102.198/443", "https://post-it-heroku.herokuapp.com"],
   },
 });
 
@@ -25,15 +41,15 @@ io.use(authSocket);
 io.on("connection", (socket) => socketServer(socket));
 
 mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("MongoDB connected");
-  }
+    process.env.MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log("MongoDB connected");
+    }
 );
 
-httpServer.listen(process.env.PORT || 4000, () => {
-  console.log("Listening");
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.use(express.json());
@@ -42,11 +58,3 @@ app.use("/api/posts", posts);
 app.use("/api/users", users);
 app.use("/api/comments", comments);
 app.use("/api/messages", messages);
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
