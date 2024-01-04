7d14e7203e32dc99ed1d8849d5c34dec42a389ac
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2024-01-04 17:10:33 +0800

    Change BASE_URL to http://146.190.102.198:4000/ (My Ubuntu Server)

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,1 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
-}
-
-export { BASE_URL };
+export const BASE_URL = "http://146.190.102.198:4000/";
